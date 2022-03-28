8b32eb60343146e6cd8debb69f680f52adf6ed17
jenalmond <jencalmond@gmail.com>
2022-03-28 12:52:34 -0600

    Add project files.

---
+++ BowlingLeague/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using BowlingLeague.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BowlersDbContext _context { get; set; }
+
+        //Constructor
+        public HomeController(BowlersDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var bowl = _context.Bowlers.ToList();
+            return View(bowl);
+        }
+
+    }
+}
